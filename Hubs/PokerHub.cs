9a56d3b6e254aa984320d6d24c43c52f4bebd2f7
Márk Polák <mwd@outlook.sk>
2019-09-20 15:16:13 +0200

    Created app basics

--- Hubs/PokerHub.cs
+++ Hubs/PokerHub.cs
@@ -1,30 +1,61 @@
-﻿using Microsoft.AspNetCore.SignalR;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using ScrummaService.Models;
 
-namespace ScrummaService.Hubs
-{
-    public class PokerHub : Hub
-    {
-        private static List<string> Connections = new List<string>();
+namespace ScrummaService.Hubs {
+    public class PokerHub : Hub {
+        private static List<User> Connections = new List<User> ();
 
-        public override async Task OnConnectedAsync()
-        {
-            if (Connections.Count > 0)
-            {
-                await Clients.Client(Connections.First()).SendAsync("clientSync", Context.ConnectionId);
-            }
-            Connections.Add(Context.ConnectionId);
+        public override async Task OnConnectedAsync () {
 
-            await base.OnConnectedAsync();
-            await Clients.All.SendAsync("clientConnected");
+            await base.OnConnectedAsync ();
+            await Clients.All.SendAsync ("clientConnected");
         }
 
-        public async Task OnVote(float num)
-        {
-            await Clients.All.SendAsync("clientVoted", Context.ConnectionId,num);
+        public override async Task OnDisconnectedAsync (Exception exception) {
+            System.Console.WriteLine ("Disconnected");
+            User user = Connections.FirstOrDefault (c => c.ConnectionId == Context.ConnectionId);
+
+            if (user != null) {
+                Connections.Remove (user);
+                await Clients.All.SendAsync ("clientLeft", user.ConnectionId);
+            }
+        }
+
+        public async Task OnJoin (string UserName) {
+            User user = new User {
+                ConnectionId = Context.ConnectionId,
+                UserName = UserName,
+                Role = RoleEnum.User,
+            };
+
+            if (Connections.Count > 0) {
+                await Clients.Others.SendAsync ("clientSyncRequest", Context.ConnectionId);
+            }
+
+            Connections.Add (user);
+
+            await Clients.Caller.SendAsync ("selfJoined", user);
+            await Clients.Others.SendAsync ("clientJoined", user);
+        }
+
+        public async Task OnVote (float num) {
+            await Clients.Others.SendAsync ("clientVoted", Context.ConnectionId, num);
+        }
+
+        public async Task OnSync (string connectionId, User user) {
+            await Clients.Client (connectionId).SendAsync ("clientSyncResponse", user);
+        }
+
+        public async Task OnTimerStart () {
+            await Clients.All.SendAsync ("clientTimerStart");
+        }
+
+        public async Task OnTimerStop () {
+            await Clients.All.SendAsync ("clientTimerStop");
         }
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ScrummaService.Models
+{
+    public enum RoleEnum
+    {
+        User,
+        Observer
+    }
+
+    public class User
+    {
+        public string ConnectionId { get; set; }
+
+        public string UserName { get; set; }
+
+        public float? Vote { get; set; }
+
+        public RoleEnum Role { get; set; } = RoleEnum.User;
+    }
+}
