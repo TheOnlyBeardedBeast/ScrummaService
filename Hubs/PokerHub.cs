1d35ceff461da18e02278a84fc112f579c414536
mwd@outlook.sk <mwd@outlook.sk>
2019-09-22 19:47:38 +0200

    Added timer sync

--- Hubs/PokerHub.cs
+++ Hubs/PokerHub.cs
@@ -5,57 +5,69 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using ScrummaService.Models;
 
-namespace ScrummaService.Hubs {
-    public class PokerHub : Hub {
-        private static List<User> Connections = new List<User> ();
+namespace ScrummaService.Hubs
+{
+    public class PokerHub : Hub
+    {
+        private static List<User> Connections = new List<User>();
 
-        public override async Task OnConnectedAsync () {
+        public override async Task OnConnectedAsync()
+        {
 
-            await base.OnConnectedAsync ();
-            await Clients.All.SendAsync ("clientConnected");
+            await base.OnConnectedAsync();
+            await Clients.All.SendAsync("clientConnected");
         }
 
-        public override async Task OnDisconnectedAsync (Exception exception) {
-            System.Console.WriteLine ("Disconnected");
-            User user = Connections.FirstOrDefault (c => c.ConnectionId == Context.ConnectionId);
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            System.Console.WriteLine("Disconnected");
+            User user = Connections.FirstOrDefault(c => c.ConnectionId == Context.ConnectionId);
 
-            if (user != null) {
-                Connections.Remove (user);
-                await Clients.All.SendAsync ("clientLeft", user.ConnectionId);
+            if (user != null)
+            {
+                Connections.Remove(user);
+                await Clients.All.SendAsync("clientLeft", user.ConnectionId);
             }
         }
 
-        public async Task OnJoin (string UserName) {
-            User user = new User {
+        public async Task OnJoin(string UserName)
+        {
+            User user = new User
+            {
                 ConnectionId = Context.ConnectionId,
                 UserName = UserName,
                 Role = RoleEnum.User,
             };
 
-            if (Connections.Count > 0) {
-                await Clients.Others.SendAsync ("clientSyncRequest", Context.ConnectionId);
+            if (Connections.Count > 0)
+            {
+                await Clients.Others.SendAsync("clientSyncRequest", Context.ConnectionId);
             }
 
-            Connections.Add (user);
+            Connections.Add(user);
 
-            await Clients.Caller.SendAsync ("selfJoined", user);
-            await Clients.Others.SendAsync ("clientJoined", user);
+            await Clients.Caller.SendAsync("selfJoined", user);
+            await Clients.Others.SendAsync("clientJoined", user);
         }
 
-        public async Task OnVote (float num) {
-            await Clients.Others.SendAsync ("clientVoted", Context.ConnectionId, num);
+        public async Task OnVote(float num)
+        {
+            await Clients.Others.SendAsync("clientVoted", Context.ConnectionId, num);
         }
 
-        public async Task OnSync (string connectionId, User user) {
-            await Clients.Client (connectionId).SendAsync ("clientSyncResponse", user);
+        public async Task OnSync(string connectionId, User user, int? seconds)
+        {
+            await Clients.Client(connectionId).SendAsync("clientSyncResponse", user, seconds);
         }
 
-        public async Task OnTimerStart () {
-            await Clients.All.SendAsync ("clientTimerStart");
+        public async Task OnTimerStart()
+        {
+            await Clients.All.SendAsync("clientTimerStart");
         }
 
-        public async Task OnTimerStop () {
-            await Clients.All.SendAsync ("clientTimerStop");
+        public async Task OnTimerStop()
+        {
+            await Clients.All.SendAsync("clientTimerStop");
         }
     }
 }
